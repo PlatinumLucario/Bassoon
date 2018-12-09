b765ee74b3a7a3bce998084016ab5cea73e45ad2
Benjamin N. Summerton <def.pri.pub@gmail.com>
2018-12-09 15:43:59 -0500

    Don't nee to set PKG_CONFIG_PATH there too

--- set_dev_env.sh
+++ set_dev_env.sh
@@ -8,7 +8,4 @@ export BASSOON_ROOT
 
 # Third Party libraries
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BASSOON_ROOT/third_party/lib
-PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BASSOON_ROOT/third_party/lib/pkgconfig
-
 export LD_LIBRARY_PATH
-export PKG_CONFIG_PATH
