a4d76ed409c5528f00d57c6a4ef366a8692565a3
Benjamin N. Summerton <def.pri.pub@gmail.com>
2018-12-20 22:39:19 -0500

    Fix building of native DLLs for Windows
    - Copies the DLLs from `bin/` to `lib/`
    - Renames PortAudio and libsndfile's DLLs to be comatabile with the
      P/Invoke code

---
+++ third_party/win_dll_fixup.cmake
@@ -0,0 +1,25 @@
+# If we are on windows, after the builds are done, we want to copy the actual
+# C#/.NET compatible native .dll file.  If you look in the `lib/` folder,
+# you'll see a bunch of `*.dll.a` files.  Those are not compatible with the
+# Windows .NET runtime.  Regular (compatable) .dlls are built, but they are
+# put into the `bin/` dir.  We copy them over to `lib/`, and rename them
+
+# The third (and only "true") argument should be our working dir, or the `third_party/`
+# directory
+set(THIRD_PARTY_DIR ${CMAKE_ARGV3})
+
+# Handy vars
+set(BIN_DIR ${THIRD_PARTY_DIR}/bin)
+set(LIB_DIR ${THIRD_PARTY_DIR}/lib)
+
+# Copy
+file(INSTALL ${BIN_DIR}/libogg-0.dll
+             ${BIN_DIR}/libvorbis-0.dll
+             ${BIN_DIR}/libFLAC-8.dll
+             ${BIN_DIR}/libsndfile-1.dll
+             ${BIN_DIR}/libportaudio-2.dll
+     DESTINATION ${LIB_DIR})
+
+# Rename
+file(RENAME ${LIB_DIR}/libsndfile-1.dll   ${LIB_DIR}/sndfile.dll)
+file(RENAME ${LIB_DIR}/libportaudio-2.dll ${LIB_DIR}/portaudio.dll)
