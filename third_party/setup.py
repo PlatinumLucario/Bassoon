3cf88ec44bf1552e23336b22a85911640ae02104
Benjamin N. Summerton <def.pri.pub@gmail.com>
2020-03-07 14:37:47 -0500

    Fix dylibs for OS X
    Aparently changing their filenames isn't enough.  You also need to
    fiddle with their id names, as well as the other dylibs that refence
    them.
    
    An extra rule/command was added to the setup script.  But now these
    packages can be made via vcpkg and all is good.

--- third_party/setup.py
+++ third_party/setup.py
@@ -96,3 +96,33 @@ for (src_name, dst_name) in dll_renaming_rules:
         os.path.join('lib/', src_name),
         os.path.join('lib/', dst_name)
     )
+
+
+# If on OS, we need to fix some issues with the dylibs
+# namely the rpath stuff & dylib IDs,  it's a bit of a pain
+if is_osx:
+    cmds = []
+
+    # First fix Ids
+    dylibs = ['ogg', 'vorbis', 'vorbisenc', 'vorbisfile', 'sndfile']
+    for lib in dylibs:
+        lib_filename = 'lib%s.dylib' % (lib)
+        cmd = 'install_name_tool -id "@rpath/{0}" lib/{0}'.format(lib_filename)
+        cmds.append(cmd)
+
+    # Now fix the references
+    changes = [
+        # (old, new, [targets])
+        ('ogg.0', 'ogg',                   ['FLAC', 'sndfile', 'vorbis', 'vorbisenc', 'vorbisfile']),
+        ('vorbis.0.4.8', 'vorbis',         ['sndfile', 'vorbisenc', 'vorbisfile']),
+        ('vorbisfile.3.3.7', 'vorbisfile', ['sndfile']),
+        ('vorbisenc.2.0.11', 'vorbisenc',  ['sndfile']),
+    ]
+    for (o, n, targets) in changes:
+        for t in targets:
+            change_cmd = 'install_name_tool -change "@rpath/lib{0}.dylib" "@rpath/lib{1}.dylib" lib/lib{2}.dylib'.format(o, n, t)
+            cmds.append(change_cmd)
+
+    # Run the commands
+    [os.system(x) for x in cmds]
+
